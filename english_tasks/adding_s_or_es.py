48d678ad31a31f06f23bf2c998cd1535cb583381
danilalabydindfsdf <labydin2605@mail.ru>
2021-12-11 18:03:55 +0300

    move these files in right folders

---
+++ english_tasks/adding_s_or_es.py
@@ -0,0 +1,22 @@
+import re
+verbs = ['swim', 'read', 'teach', 'sex', 'kiss', 'fish']
+
+
+def add_ending(word):
+    """
+    The function add the ending to the vern by english grammar
+    :param word: the V1 verb
+    :return: the V1 with right ending
+    """
+    if re.search(r'ss|ch|sh|x', word):
+        return word + 'es'
+    return word + 's'
+
+
+def main():
+    for verb in verbs:
+        print(add_ending(verb))
+
+
+if __name__ == '__main__':
+    main()
