ad4143ac0ac2ddb04c31a2ece0bf6af295e92221
danilalabydindfsdf <labydin2605@mail.ru>
2021-12-26 22:59:26 +0300

    fixed bugs, rewrite functions
    add DictReader methed to the get_verbs_list function
    delete regex from compare_verb function
    split dispay_statistics function to 3 separate functions(compute scores, display and save stat)
    delete randon_verb and check_answers function
    add generate_sub_verbslist function to extract some verbs from verbslist
    add shuffle method

--- english_tasks/irregular_verbs.py
+++ english_tasks/irregular_verbs.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-##
-# create a program that generates a random sentence for english practice
-#
-import re
 import random
 import csv
 import time
@@ -11,31 +7,26 @@ import sys
 from argparse import ArgumentParser
 
 
-# create lists to store into them verbs for stat
-RESULT_DICT = {'correct_verbs': [], 'wrong_verbs': [], 'fixed_verbs': []}
-
-
 def greeting():
     print('This exercise help you learn irregular english verbs!\n'
           'Some rules that you should know\n'
           '    Write only V2 and V3 forms of verb\n'
           '    You have only 2 attempts to write the verb correctly\n'
-          'Now enter the number of verbs\n')
+          'Now enter the number of verbs to practice\n')
 
 
 def get_verbs_list(file):
-    """
-    the function read a file and returns the list of file's rows
-    :param file: the current file(name or path)
-    :return: the list that contains file's rows in tuple format
-    """
+    """the function read a file and returns the list of file's rows"""
     rows = []
+
     try:
-        with open(file) as file:
+        with open(file) as f:
             # open file like csv file because we want to store each value that is separated by a comma
-            csv_file = csv.reader(file)
-            for row in csv_file:
-                rows.append(row)
+
+            csv_f = csv.DictReader(f)
+            for row in csv_f:
+                rows.append([row['first_form'], row['second_form'],
+                             row['third_form'], row['translate']])
 
             return rows
 
@@ -44,129 +35,146 @@ def get_verbs_list(file):
         exit(1)
 
 
-def random_verb(list_of_verbs: list):
-    """
-    The function creates a random verb from the list
-    :param list_of_verbs:
-    :return:
-    """
-    random_index = random.randint(0, len(list_of_verbs) - 1)
-    verbs = list_of_verbs[random_index]
-    return verbs
+def compare_verb(user_input, current_verb):
+    """The function compares user's input with certain verb"""
+    if user_input.strip().lower() == current_verb:
+        return True
+    return False
 
 
-def compare_verb(user_input: str, current_verb: str):
-    """
-    The function compares user's input with certain verb
-    :param user_input: a verb that was entered by the user
-    :param current_verb: the current verb(V3 or V3)
-    :return: True if the user wrote the correct form, otherwise False
-    """
-    pattern = current_verb
-    result = re.search(pattern, user_input)
-    if result is None:
-        return False
+def compute_scores(verb_dict):
+    """The function computes the number of user's scores"""
+    correct_answers = len(verb_dict['correct'])
+    fixed_answers = len(verb_dict['fixed'])
+    wrong_answers = len(verb_dict['wrong'])
+    total = correct_answers + fixed_answers + wrong_answers
+    scores = ((correct_answers + fixed_answers * 0.5) / total) * 100
+    return scores
 
-    return True
+
+def display_stat(user_name, verb_quantity, verb_dict):
+    print('\n\tYOUR STAT')
+    print(f'Name - {user_name}\n'
+          f'Amount of verb - {verb_quantity}\n'
+          f'Scores - {compute_scores(verb_dict)}%')
+    for key, value in verb_dict.items():
+        print(f'{key} - {value}')
 
 
 def save_statistics(file_name, name, verb_quantity, verb_dict):
-    """The function displays all statistic"""
-    # compute the rating system and store it into a file
-    total = 0
-    correct_answers = len(verb_dict['correct_verbs'])
-    fixed_answers = len(verb_dict['fixed_verbs'])
-    for key, value in verb_dict.items():
-        total += len(value)
-
-    scores = ((correct_answers + fixed_answers * 0.5) / total) * 100
-    now_time = time.asctime()
-
-    user_stat = f'{name.title()} | {now_time} | ({verb_quantity} verbs) | {scores}%\n'
+    """The function displays all statistics in a given file"""
+    user_stat = f'{name.title()} | {time.asctime()} | ({verb_quantity} verbs) | {compute_scores(verb_dict)}%\n'
     with open(file_name, 'a') as file:
         file.write(user_stat)
-
-    # display lists of verbs
-    for key, value in RESULT_DICT.items():
-        print(f'{key} - {value}')
 
 
 def get_args():
     a = ArgumentParser()
     a.add_argument('-f', '--filename', dest='verbs_filename', type=str, required=True)
-    a.add_argument('-o', '--name', dest='stat_filename', type=str, required=False)
+    a.add_argument('-o', '--out', dest='stat_filename', type=str, required=False)
     return a.parse_args()
 
 
-def generate_attempts(nb_attempt, verb, form_nb):
+def generate_attempts(nb_attempt, verb):
+    """
+    The function gives the user a certain number of attempts to rewrite his answer, if user's new answer is right,
+    return True, otherwise False
+    """
     for attempt in range(1, nb_attempt + 1):
         print(f'Attempt №{attempt}')
-        next_answer = input(f'Enter V{form_nb} again: ')
+        next_answer = input('Enter: ')
         if compare_verb(next_answer, verb):
             print('Correct!')
-            RESULT_DICT['fixed_verbs'].append(verb)
             return True
-    RESULT_DICT['wrong_verbs'].append(verb)
     return False
 
 
-def check_answers(irregular_verb, result_v2, result_v3):
-    """
-    Check if the answers are correct, if all answers are correct, store the infinitive of verb in dictionary(correct_v)
-    otherwise give the user attempts to write the answer correctly
-    """
-    if result_v2 and result_v3:
-        print('All form is correct!\n')
-        RESULT_DICT['correct_verbs'].append(irregular_verb[0])
-        return
-
-    # check if the v2 is incorrect
-    if not result_v2:
-        print('\nV2 is wrong!')
-        if not generate_attempts(2, irregular_verb[1], 2):
-            print(f'The correct form - {irregular_verb[1]}')
-
-    # check if the v3 is incorrect
-    if not result_v3:
-        print('\nV3 is wrong!')
-        if not generate_attempts(2, irregular_verb[2], 3):
-            print(f'The correct form - {irregular_verb[2]}')
-    return
+def generate_sub_verbslist(verbs_list, item_quantity):
+    """The function generates a list from part of an other list"""
+    sub_list = verbs_list[:item_quantity]
+    return sub_list
 
 
 def main():
+    # get arguments from command line
     arguments = get_args()
 
     # read the special file to store verbs into a list
     verbs_list = get_verbs_list(arguments.verbs_filename)
+
+    # display the greeting message that contains the rules of the program
     greeting()
-    name = input('Enter your name: ')
-    nb_verb = int(input('Enter the number of verbs: '))
+    user_name = input('Enter your name: ')
 
-    if not 0 < nb_verb <= len(verbs_list):
-        print('Error! You entered a wrong value (max number is 74)')
-        print(len(verbs_list))
-        sys.exit(1)
+    while True:
+        verb_quantity = int(input('Enter the number of verb to practice: '))
+        if not 0 < verb_quantity <= 75:
+            print('Error! You entered a wrong value (max number is 75)')
+            sys.exit(1)
 
-    for verb in range(1, nb_verb + 1):
-        print(f'\nverb №{verb}')
-        current_verb = random_verb(verbs_list)
-        print(current_verb[0])
+        # mix the content of a list
+        random.shuffle(verbs_list)
 
-        # read the answers from the user and compare it with correct v2 and v3 forms
-        answer_v2 = input('Enter the V2: ')
-        answer_v3 = input('Enter the V3: ')
-        result_v2 = compare_verb(answer_v2, current_verb[1])
-        result_v3 = compare_verb(answer_v3, current_verb[2])
+        # extract the certain number of verb to the sub_verbs_list from the main list
+        sample_verbs = generate_sub_verbslist(verbs_list, verb_quantity)
 
-        # check the answers and delete the verb from the list
-        check_answers(current_verb, result_v2, result_v3)
-        verbs_list.remove(current_verb)
+        # create a dict to store each verb in the separated group (correct; wrong;fixed) verbs
+        sorted_verbs = {'correct': [], 'wrong': [], 'fixed': []}
 
-    # display all statistic and store the result into a file
-    if arguments.stat_filename:
-        save_statistics(arguments.stat_filename, name, nb_verb, RESULT_DICT)
+        # iterate each verb in the list and display the number of iteration
+        count = 0
+
+        for verb in sample_verbs:
+            count += 1
+            print(f'\nThe verb №{count}\n')
+            print(f'{verb[0]} - {verb[3]}')
+
+            # read the answers from the user and compare it with correct v2 and v3 forms
+            answer_v2 = input('Enter the V2: ')
+            answer_v3 = input('Enter the V3: ')
+            result_v2 = compare_verb(answer_v2, verb[1])
+            result_v3 = compare_verb(answer_v3, verb[2])
+
+            # if all answers are right, store infinitive with appropriate key and continue iterating,
+            # otherwise give two attempts to user to rewrite their answers
+
+            if result_v2 and result_v3:
+                sorted_verbs['correct'].append(verb[0])
+                print('All forms are correct!')
+                continue
+
+            # create a variable to store the result of rewriting, and give the chance to rewrite if the answer is wrong
+            second_chance = []
+            if not result_v2:
+                print('\nV2 is wrong!')
+                second_chance.append(generate_attempts(2, verb[1]))
+
+            if not result_v3:
+                print('\nV3 is wrong!')
+                second_chance.append(generate_attempts(2, verb[2]))
+
+            # if all attempts are right, then add the verb into a fixed group, otherwise add into a wrong group
+
+            if all(second_chance):
+                sorted_verbs['fixed'].append(verb[0])
+                continue
+
+            sorted_verbs['wrong'].append(verb[0])
+            print(f'Correct forms: {verb[1]}, {verb[2]}\n')
+
+        # display the practice results
+        display_stat(user_name, verb_quantity, sorted_verbs)
+
+        # if user entered the save file, then save info in it
+        if arguments.stat_filename:
+            save_statistics(arguments.stat_filename, user_name, verb_quantity, sorted_verbs)
+
+        # ask the user about next practice
+        ask_again = input('Do you want to complete this task again(q to quit): ')
+        if ask_again == 'q':
+            break
 
 
 if __name__ == '__main__':
     main()
+    
