8af0af6cbe43335054c7ba845a424525b10115bc
danilalabydindfsdf <labydin2605@mail.ru>
2021-12-29 21:53:39 +0300

    refact compare_verb func, use dict in get_verb_list func, refact generate_attempt func, add generate_reanswer func

--- english_tasks/irregular_verbs.py
+++ english_tasks/irregular_verbs.py
@@ -18,18 +18,17 @@ def greeting():
 def get_verbs_list(file):
     """the function read a file and returns the list of file's rows"""
     rows = []
-
     try:
         with open(file) as f:
             # open file like csv file because we want to store each value that is separated by a comma
-
             csv_f = csv.DictReader(f)
             for row in csv_f:
-                rows.append([row['first_form'], row['second_form'],
-                             row['third_form'], row['translate']])
-
+                rows.append({'first_form': row['first_form'],
+                             'second_form': row['second_form'],
+                             'third_form': row['third_form'],
+                             'translate': row['translate']
+                             })
             return rows
-
     except Exception as e:
         print(e)
         exit(1)
@@ -37,9 +36,7 @@ def get_verbs_list(file):
 
 def compare_verb(user_input, current_verb):
     """The function compares user's input with certain verb"""
-    if user_input.strip().lower() == current_verb:
-        return True
-    return False
+    return user_input.strip().lower() == current_verb
 
 
 def compute_scores(verb_dict):
@@ -75,17 +72,18 @@ def get_args():
     return a.parse_args()
 
 
-def generate_attempts(nb_attempt, verb):
+def generate_attempts(verb):
     """
     The function gives the user a certain number of attempts to rewrite his answer, if user's new answer is right,
     return True, otherwise False
     """
-    for attempt in range(1, nb_attempt + 1):
+    for attempt in range(1, 3):
         print(f'Attempt №{attempt}')
-        next_answer = input('Enter: ')
-        if compare_verb(next_answer, verb):
+        answer = input('Enter again: ')
+        if compare_verb(answer, verb):
             print('Correct!')
             return True
+        print('Not correct!')
     return False
 
 
@@ -93,6 +91,18 @@ def generate_sub_verbslist(verbs_list, item_quantity):
     """The function generates a list from part of an other list"""
     sub_list = verbs_list[:item_quantity]
     return sub_list
+
+
+def generate_reanswer(boolean_list, verb_second_form, verb_third_form):
+    """The function checks rewrite answers and store them in a boolean list"""
+    results = []
+    if not boolean_list[0]:
+        print('\nV2 is wrong!')
+        results.append(generate_attempts(verb_second_form))
+    if not boolean_list[1]:
+        print('\nV3 is wrong!')
+        results.append(generate_attempts(verb_third_form))
+    return results
 
 
 def main():
@@ -108,7 +118,7 @@ def main():
 
     while True:
         verb_quantity = int(input('Enter the number of verb to practice: '))
-        if not 0 < verb_quantity <= 75:
+        if not 0 < verb_quantity <= 74:
             print('Error! You entered a wrong value (max number is 75)')
             sys.exit(1)
 
@@ -121,46 +131,30 @@ def main():
         # create a dict to store each verb in the separated group (correct; wrong;fixed) verbs
         sorted_verbs = {'correct': [], 'wrong': [], 'fixed': []}
 
-        # iterate each verb in the list and display the number of iteration
-        count = 0
-
-        for verb in sample_verbs:
-            count += 1
+        for count, verb in enumerate(sample_verbs, start=1):
             print(f'\nThe verb №{count}\n')
-            print(f'{verb[0]} - {verb[3]}')
+            print(f'{verb["first_form"]} - {verb["translate"]}')
 
             # read the answers from the user and compare it with correct v2 and v3 forms
-            answer_v2 = input('Enter the V2: ')
-            answer_v3 = input('Enter the V3: ')
-            result_v2 = compare_verb(answer_v2, verb[1])
-            result_v3 = compare_verb(answer_v3, verb[2])
+            # store the results in special list
+            answer_v2 = input('Enter V2: ')
+            answer_v3 = input('Enter V3: ')
+            results = [compare_verb(answer_v2, verb['second_form']),
+                       compare_verb(answer_v3, verb['third_form'])]
 
-            # if all answers are right, store infinitive with appropriate key and continue iterating,
-            # otherwise give two attempts to user to rewrite their answers
-
-            if result_v2 and result_v3:
-                sorted_verbs['correct'].append(verb[0])
+            # if all answers are right, store infinitive with appropriate key and continue iterating
+            if all(results):
+                sorted_verbs['correct'].append(verb['first_form'])
                 print('All forms are correct!')
                 continue
 
-            # create a variable to store the result of rewriting, and give the chance to rewrite if the answer is wrong
-            second_chance = []
-            if not result_v2:
-                print('\nV2 is wrong!')
-                second_chance.append(generate_attempts(2, verb[1]))
-
-            if not result_v3:
-                print('\nV3 is wrong!')
-                second_chance.append(generate_attempts(2, verb[2]))
-
-            # if all attempts are right, then add the verb into a fixed group, otherwise add into a wrong group
-
-            if all(second_chance):
-                sorted_verbs['fixed'].append(verb[0])
+            # give to the user the chance to rewrite their wrong answers, if all rewrite answers are right
+            # store the verb in fixed group, otherwise in wrong group
+            if all(generate_reanswer(results, verb['second_form'], verb['third_form'])):
+                sorted_verbs['fixed'].append(verb['first_form'])
                 continue
-
-            sorted_verbs['wrong'].append(verb[0])
-            print(f'Correct forms: {verb[1]}, {verb[2]}\n')
+            sorted_verbs['wrong'].append(verb['first_form'])
+            print(f'\nCorrect forms: {verb["second_form"]}, {verb["third_form"]}')
 
         # display the practice results
         display_stat(user_name, verb_quantity, sorted_verbs)
@@ -177,4 +171,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
