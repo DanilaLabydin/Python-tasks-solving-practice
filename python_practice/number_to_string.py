92c0279bb914e66efecff5f3881b6e64ba1eefef
danilalabydindfsdf <labydin2605@mail.ru>
2023-02-22 00:00:21 +0300

    add a few simple tasks from codewars

---
+++ python_practice/number_to_string.py
@@ -0,0 +1,6 @@
+# We need a function that can transform a number (integer) into a string.
+def number_to_string(num):
+    return str(num)
+
+num = input('Enter a number: ')
+print(number_to_string(num))
---
+++ python_practice/string_to_array.py
@@ -0,0 +1,10 @@
+#Write a function to split a string and convert it into an array of words.
+
+def string_to_array(s):
+    if len(s) == 0:
+        return [""]
+    return s.split()
+
+
+string = input('Enter a string: ')
+print(string_to_array(string))
