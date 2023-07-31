4d9bcfd40005ea66e1605130419c5b5a50961fe6
DanilaLabydin <labydin2605@mail.ru>
2023-07-31 23:45:59 +0300

    add alg task

--- algorithms/122_best_time_to_buy_stock.py
+++ algorithms/122_best_time_to_buy_stock.py
@@ -2,36 +2,20 @@ from typing import List
 
 
 class Solution:
-    def get_max_profit(self, prices: List[int]) -> int:
-        best = 0
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
         day_buy = prices[0]
         for day_sell in prices[1:]:
             if day_sell > day_buy:
-                best = max(best, day_sell - day_buy)
-            else:
-                day_buy = day_sell
-        return best
+                max_profit += day_sell - day_buy
+            day_buy = day_sell
 
+        return max_profit
 
-    def maxProfit(self, prices: List[int]) -> int:
-        best = 0
-        day_buy = 0
-        for i in range(len(prices)):
-            if prices[i] > prices[day_buy]:
-
-                while i < len
-                first_deal_profit = prices[i] - prices[day_buy]
-                second_deal_profit = self.get_max_profit(prices[i:])
-
-                best = max(best, first_deal_profit + second_deal_profit)
-            else:
-                day_buy = i
-             
-        return best
 
 Test = Solution()
 array1 = [7, 1, 5, 3, 6, 4]
 array2 = [7, 6, 4, 3, 1]
-print(Test.maxProfit(array2))
+array3 = [3,3,5,0,0,3,1,4]
 
-+79602551601 - Рома
+print(Test.maxProfit(array3))
