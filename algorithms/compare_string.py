9f89f1b0b8a1fd84da966c3da5d39ee363304cda
DanilaLabydin <labydin2605@mail.ru>
2023-08-11 07:07:25 +0300

    add two yandex tasks

---
+++ algorithms/compare_string.py
@@ -0,0 +1,38 @@
+string_nb = int(input())
+strings_list = [str(input()) for _ in range(2 * string_nb)]
+
+# print()
+# print(string_nb)
+# print(strings_list)
+
+def check_strings(s1, s2):
+    if len(s1) != len(s2):
+        return  'NO'
+    s1_dict = {}
+    s2_dict = {}
+
+    for i in range(len(s1)):
+        if s1_dict.setdefault(s1[i], s2[i]) != s2[i]:
+            return "NO"
+        
+        if s2_dict.setdefault(s2[i], s1[i]) != s1[i]:
+            return "NO"
+
+    return 'YES'
+
+
+s1_list = strings_list[::2]
+s2_list = strings_list[1::2]
+
+
+for i in range(len(s1_list)):
+    # if len(s1_list[i]) != len(s2_list[i]):
+    #     print("NO")
+    # elif len(set(s1_list[i])) == len(set(s2_list[i])):
+    #     print("YES")
+    # else:
+    #     print("NO")
+    print(check_strings(s1_list[i], s2_list[i]))
+
+# print(s1_list)
+# print(s2_list)
---
+++ algorithms/fake_log.py
@@ -0,0 +1,49 @@
+import datetime
+
+
+nb = int(input())
+time_log = [(input()) for i in range(nb)]
+
+days_nb = 1
+first_log_record = time_log[0]
+for i in range(1, len(time_log)):
+    curr_log = time_log[i]
+    curr_time = datetime.datetime.strptime(curr_log, '%H:%M:%S')
+
+    prev_log = time_log[i - 1]
+    prev_time = datetime.datetime.strptime(prev_log, '%H:%M:%S')
+
+    if curr_time <= prev_time:
+        days_nb += 1
+
+print(days_nb)
+
+
+
+    # print(time_log[i])
+    # print(type(time_log[i]))
+    # print(i)
+    # time = datetime.datetime(str(time_log[i]), '%H:%M:%S')
+    # print(time)
+    # hour, minute, second = time_log[i]
+
+    # if int(hour) < int(time_log[i-1][0]):
+    #     days_nb += 1
+    #     continue
+    
+    # else:
+    #     if int(minute) < int(time_log[i-1][1]):
+    #         days_nb += 1
+    #         continue
+
+    #     else:
+    #         if int(second) <= int(time_log[i-1][2]):
+
+
+    
+    
+
+
+    # if int(hour) <= int(time_log[i-1][0]) and int(minute) <= int(time_log[i-1][1]) and int(second) <= int(time_log[i-1][2]):
+    #     days_nb += 1
+    # print(time_log[i])
