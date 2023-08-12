0251fa2e4eae4b53d5f292fc6e4d15c7c96a2d87
DanilaLabydin <labydin2605@mail.ru>
2023-08-12 23:53:04 +0300

    flat list task

---
+++ algorithms/flat_list.py
@@ -0,0 +1,17 @@
+a = [1, 2, 3, [4, 5, 6, [7, 8], 9, 10], 11, 12, [13, 14]]
+# a = [3, [4, 5], 6]
+b = []
+
+def flat_list(array):
+    for i in array:
+        print(i)
+        if isinstance(i, list):
+            flat_list(i)
+        else:
+            b.append(i)
+    return b
+    
+
+flat_list(a)
+print()
+print(b)
